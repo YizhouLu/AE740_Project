cf46886de9425bd3b670be36a5ebdac8a5e65236
Yizhoulu <luyizhou@umich.edu>
2019-12-09 10:41:18 -0500

    initialize myQP

---
+++ Yizhou_QP/OL_Simulation.m
@@ -0,0 +1,154 @@
+clc; clear; close all
+
+% Load all electrical parameters
+load('SAMmodel.mat')
+
+Temp = 25; % degC
+SOC = model.SOC;
+OCV = model.OCV0 + Temp*model.OCVrel;
+
+dt = 0.001;      % sampling time
+alpha0 = 3.613;  % yintercept of SOC-OCV approximation
+alpha1 = 0.4631; % slope of SOC-OCV curve between 20% and 80% SOC
+
+R0  = getParamESC('R0Param',25,model); % Ohmic resistance
+R1  = getParamESC('RParam',25,model);  % RC circuit resistance
+tau = getParamESC('RCParam',25,model); % RC time constant
+C1 = tau/R1;                           % RC circuit capacitance
+Q = getParamESC('QParam',25,model);    % Battery capacity
+
+% Discrete electrical dynamics
+
+% [delta_z; delta_Vc; i_(k - 1); e; z; Vc; 1]
+A = [   1,                0, 0, 0, 0, 0, 0;
+        0, exp(-dt/(R1*C1)), 0, 0, 0, 0, 0;
+        0,                0, 1, 0, 0, 0, 0;
+        1,                0, 0, 1, 0, 0, 0;
+        1,                0, 0, 0, 1, 0, 0;
+        0,                1, 0, 0, 0, 1, 0;
+        0,                0, 0, 0, 0, 0, 1];
+    
+B = [                   -dt/Q;
+      R1*(1-exp(-dt/(R1*C1)));
+                            1;
+                            0;
+                            0;
+                            0;
+                            0];
+
+% [e, Vt, i, z]                  
+C = [0, 0,   0, 1,      0,  0,      0;
+     0, 0, -R0, 0, alpha1, -1, alpha0;
+     0, 0,   1, 0,      0,  0,      0;
+     0, 0,   0, 0,      1,  0,      0];
+  
+D = [0; -R0; 1; 0];
+
+sys_d = ss(A,B,C,D);
+
+% Set parameters for the MPC problem
+Np = 10;  % prediction horizon
+Nc = 1;   % control horizon
+r  = 0.5; % control weighting on deltaCurrent
+
+% set the constraints
+lN = 100; % large number for outputs that don't need to be constrained
+
+% we have no constraints on deltaU but HW solutions still have it input
+% into the constraint formulation so I'll do the same.
+delta_ulimit.max = lN;
+delta_ulimit.min = -delta_ulimit.max;
+
+% constrain the terminal voltage with manufacturer's specs which are just
+% open circuit voltage at 0% SOC and 100% SOC at 25 deg C
+OCV_low = GetOCV(0.2,Temp,model);  % OCV which corresponds to 0.2% SOC 
+OCV_high = GetOCV(0.8,Temp,model); % OCV which corresponds to 80% SOC
+
+% Q and -Q are used for the current constraints indicating a 1C
+% charge/discharge rate meaning the battery will be fully
+% discharged/charged in 1 hour
+
+% extended output: [e, Vt, i, z]
+ylimit.max = [ lN; OCV_high; 0; 0.8];
+ylimit.min = [-lN; OCV_low; -Q; 0.2];
+
+[H,L,G,W,T] = formQPMatrices(A, B, C, D, Np, Nc, r, ylimit, delta_ulimit);
+lambda0 = ones(size(G,1),1);
+
+Nsim = 1500;
+
+% initial conditions of the extended state
+% [delta_z; delta_Vc; i_(k - 1); e; z; Vc; 1]
+X0 = [0; 0; 0; -0.6; 0.2; 0; 1]; 
+
+X = X0;
+delta_u0 = 0; % Initial DeltaU is zero
+delta_u = delta_u0;
+y0 = C*X0 + D*delta_u0;
+Y = y0;
+
+Control = zeros(1,Nsim);
+
+SOC_setpoint = 0.8;
+
+for i = 1:Nsim
+ 
+    X0(4) = X0(5) - SOC_setpoint;
+    
+    Wtilde = W + T*X0;
+    q = L*X0;
+    
+    %[DeltaU, lambda] = myQP(H, q, G, Wtilde, lambda0);
+    [DeltaU,~,~,~,lambda] = quadprog(H,q,G,Wtilde);
+    lambda0 = lambda;
+
+    delta_u = DeltaU(1);  % delta u
+    u = delta_u + X0(3);  % actual control actuation
+    Control(1,i) = u;
+
+    X(:,i+1) = A*X(:,i) + B*delta_u; % [delta_z; delta_Vc; i_(k - 1); e; z; Vc; 1]
+    Y(:,i)   = C*X(:,i) + D*delta_u; % [e, Vt, i, z]
+
+    X0 = X(:,end);
+    
+end
+
+t = 1:1:Nsim; % sampling instant
+
+figure('Name','Fast Charge Output');
+set(gcf,'Color','White','Units','Normalized','Position',[0.2 0.2 0.6 0.6]);
+
+h_ax1(1) = subplot(221); % error
+plot(t,Y(1,:),'b','LineWidth',2);
+% no constraints on error so not plotted
+xlabel('Time [s]');
+legend('error','Location','best');
+set(gca,'FontSize',14);
+
+h_ax1(2) = subplot(222); % voltage
+plot(t,Y(2,:),'b','LineWidth',2);
+hold on 
+plot(t,ylimit.max(2)*ones(size(t)),'--r','LineWidth',2);
+plot(t,ylimit.min(2)*ones(size(t)),'--r','LineWidth',2);
+xlabel('Time [s]');
+legend('terminal voltage','Location','best');
+set(gca,'FontSize',14);
+
+h_ax1(1) = subplot(223); % current
+plot(t,Y(3,:),'b','LineWidth',2);
+hold on
+plot(t,ylimit.max(3)*ones(size(t)),'--r','LineWidth',2);
+plot(t,ylimit.min(3)*ones(size(t)),'--r','LineWidth',2);
+xlabel('Time [s]');
+legend('current','Location','best');
+set(gca,'FontSize',14);
+
+h_ax1(1) = subplot(224); % SOC
+plot(t,Y(4,:),'b','LineWidth',2);
+hold on
+plot(t,ylimit.max(4)*ones(size(t)),'--r','LineWidth',2);
+plot(t,ylimit.min(4)*ones(size(t)),'--r','LineWidth',2);
+xlabel('Time [s]');
+legend('State of Charge','Location','best');
+set(gca,'FontSize',14);
+linkaxes(h_ax1,'x');
---
+++ Yizhou_QP/formQPMatrices.m
@@ -0,0 +1,147 @@
+function [H,L,G,W,T] = formQPMatrices(A, B, C, D, Np, Nc, r, ylimit, Delta_ulimit)
+
+% Inputs: A, B, C, D are the original state space matrices of the system
+% dynamics. X0 is the set of initial conditions for the EXTENDED state
+% vector which accounts for the direct feedthrough matrix D. Np is the
+% prediction horizon. Nc is the control horizon. Np >= Nc. r is the
+% weighting for the DeltaU in the cost function -> must be nonzero so that
+% we have a positive definite matrix I think.. SP is the set point we wish
+% to track (ie. set point for the State of Charge).
+
+% Outputs: H is the Hessian. q is the linear term of the cost function. G
+% is the part of the constraint matrix multiplying DeltaU. W is the matrix
+% of stacked Xmax, Xmin, Ymax and Ymin constraints. T is the affine portion
+% of the constraint which is multiplied by X0.
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% % First form the modified A, B and C matrices to account for
+% % the direct feedthrough term arising from the D matrix. Tried to
+% % generalize for arbitrary input case. Time will tell if it worked lol.
+% 
+% A = [A, B; zeros(size(B,2),size(A,2)), eye(size(B,2))];
+% B = [zeros(size(B,1),1); ones(size(B,2),1)];
+% C = [C, D];
+% 
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% % X = M*x0 + S*DeltaU
+% 
+% M = [];
+% for i = 1:Np
+%     M = [M;A^i];
+% end
+% 
+% col_S = [];
+% for i = 1:Np
+%     col_S = [col_S;(A^(i - 1)*B)];
+% end
+% 
+% S = zeros(Np*size(A,1),Nc*size(B,2));
+% 
+% for i = 1:Nc
+%     if i == 1
+%         S = col_S;
+%     else
+%         col_S = [zeros(size(B,1),1);col_S]; 
+%         col_S((end - size(B,1)) + 1:end,:) = [];
+%         S = [S,col_S];
+%     end
+% end
+
+%%%%%%%%%%%%%%%
+% Y = Phi*x0 + Gamma*DeltaU
+
+Phi = [];
+for i = 1:Np
+    Phi = [Phi;C*A^(i)];
+end
+
+% % we actually need Phi*A in the new formulation so spit that out of
+% % the function instead of just Phi
+% 
+% Phi_mult_A = Phi*A;
+
+col_Gamma = [];
+for i = 1:(Np + 1)
+    if i == 1
+        col_Gamma = D;
+    else
+        col_Gamma = [col_Gamma;C*(A^(i - 2))*B];
+    end
+end
+
+Gamma = zeros(size(C,1)*(Np + 1),(Nc*size(B,2)));
+for i = 1:Nc
+    if i == 1
+        Gamma = col_Gamma;
+    else
+        col_Gamma = [zeros(size(C,1),1);col_Gamma]; 
+        col_Gamma((end - size(C,1)) + 1:end,:) = [];
+        Gamma = [Gamma,col_Gamma];
+    end
+end
+
+Gamma = Gamma(size(C,1)+1:end,:);
+
+% Q bar is a symmetric & positive-semi definite matrix. We wish to penalize
+% the error between measured SOC and SOC set point. The output vector is 
+% [e, Vt, i, z]. Here the error is plucked out by Q so that it is
+% penalized, while Vt, i and z are all constrained outputs.
+Q = diag([1.5, 0, 0, 0]);
+Qbar = blkdiag(Q);
+
+while size(Qbar) ~= Np*size(C,1)
+    Qbar = blkdiag(Q,Qbar);
+end
+
+% Rbar is a symmetric & positive definite matrix. It penalizes the the
+% control increment by value r.
+Rbar = r*eye(Nc);
+
+%%%%%%%%%%%%%%%
+% Cost Function
+
+H = 2.*(Gamma'*Qbar*Gamma + Rbar);
+L = 2.*(Gamma'*Qbar*Phi);
+
+% SP = SP*ones(size(Gamma,1),1);
+% H = 2.*(Gamma'*Qbar*Gamma + r);
+% q = 2.*Gamma'*Qbar*(Phi_mult_A*X0 - SP);
+% % we might need to move X0 outside the function since that's what we did
+% % for HW3 P2, but here we have two terms so we might have to output both if
+% % we go that route
+
+
+%%%%%%%%%%%%%%%
+% Handle constraints
+
+% should the DeltaU constrain be Np x Nc or just Nc x Nc? If the control
+% horizon is 3 and prediction horizon is 10, DeltaU will be constant for
+% steps 4, 5, 6, 7, 8, 9, 10 so I think we only have to constrain the first
+% three.. not sure though.
+% G = [eye(Nc); -eye(Nc)];
+G = [eye(Nc); -eye(Nc); Gamma; -Gamma];
+
+DeltaUmax = [];
+DeltaUmin = [];
+Ymax = [];
+Ymin = [];
+
+for i = 1:Nc
+    DeltaUmax = [DeltaUmax;Delta_ulimit.max];
+    DeltaUmin = [DeltaUmin;Delta_ulimit.min];
+end 
+
+while size(Ymin,1) ~= size(Gamma,1)
+    Ymax = [Ymax;ylimit.max];
+    Ymin = [Ymin;ylimit.min];
+end 
+
+% W = [DeltaUmax; -DeltaUmin];
+% T = [zeros(Nc,size(A,1)); zeros(Nc,size(A,1))];
+
+W = [DeltaUmax; -DeltaUmin; Ymax; -Ymin];
+T = [zeros(Nc,size(A,1)); zeros(Nc,size(A,1)); 
+    -Phi; Phi];
+
+end
