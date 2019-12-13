8bef886e1301e8e5b94a14f849bfe9e47e81c1a6
mstojcic <mstojcic@umich.com>
2019-12-13 16:43:08 -0500

    update with all most recent scripts and functions

---
+++ Yizhou_QP/generateElecPackModel.m
@@ -0,0 +1,72 @@
+function [A_aug, B_aug, C_aug, D_aug] = generateElecPackModel(Temp, SOC, model, dt, coe)
+
+OCV =  polyval(coe,SOC);
+
+R0  = getParamESC('R0Param',Temp,model); % Ohmic resistance
+R1  = getParamESC('RParam',Temp,model);  % RC circuit resistance
+tau = getParamESC('RCParam',Temp,model); % RC time constant
+C1 = tau/R1;                             % RC circuit capacitance
+
+num_series        = 96;
+num_parallel_cell = 20;
+num_strings       = 14;
+
+
+Q_cell = getParamESC('QParam',Temp,model); % Battery capacity
+Q_pack = num_parallel_cell * num_strings * Q_cell;    
+
+
+% Series Resistance
+Rtab      = 0.000125;    % 125 microOhm resistance for each tab
+R0_eq     = R0 + 2*Rtab; % add tab resistance to cell resistance
+R0_brick  = R0_eq./num_parallel_cell;
+R0_string = R0_brick.*num_series;
+R0_pack   = R0_string./num_strings;
+
+
+
+A = [0,         0;
+      0,-1/(R1*C1)];
+B = [ -1/(Q_pack*3600);
+       1/C1];
+C = zeros(1,2);
+D = 0;
+
+A2 = [0,         0,  0;
+      0,-1/(R1*C1),  0;
+      0,         0,  0];
+B2 = [ -1/(Q_pack*3600);
+       1/C1;
+         0];
+C2 = [0, -1, OCV];
+D2 = -R0_pack;
+
+sys1_d = c2d(ss(A,B,C,D), dt);
+sys2_d = c2d(ss(A2,B2,C2,D2), dt);
+
+% state = [delta_z; delta_Vc; z; Vc; 1; i_last; e]
+
+A_aug = zeros(7);
+A_aug(1:2,1:2) = sys1_d.A;
+A_aug(3:5,3:5) = sys2_d.A;  
+A_aug(3:5,6)   = sys2_d.B;  
+A_aug(6,:)     = [0, 0, 0, 0, 0, 1, 0];
+A_aug(7,:)     = [1, 0, 0, 0, 0, 0, 1];
+
+% input = delta_i
+B_aug = [sys1_d.B;
+         sys2_d.B;
+                1;
+                0];
+            
+% output = [Vt, e, i, z]                  
+C_aug = [0, 0, sys2_d.C, sys2_d.D, 0; 
+         0, 0,  0, 0, 0,        0, 1;
+         0, 0,  0, 0, 0,        1, 0;
+         0, 0,  1, 0, 0,        0, 0];
+  
+D_aug = [sys2_d.D;
+                0;
+                1;
+                0];
+end
---
+++ Yizhou_QP/generateThmlPackModel.m
@@ -0,0 +1,28 @@
+function [A_aug, B_aug, C_aug, D_aug] = generateThmlPackModel(Temp, Current, model, dt)
+
+Cc = 80;   % J/K  (tune)
+Cs = 10;   % J/K  (tune)
+Re = 1;    % Ohms (combo of R1 and R0)
+Rc = 1;    % K/W  (tune)
+Ru = 4;    % K/W  (tune)
+
+num_parallel_cell = 20;
+num_strings       = 14;
+
+Current = Current / (num_parallel_cell*num_strings);
+
+A = [  -1/(Rc*Cc),  1/(Rc*Cc);
+        1/(Cs*Rc), -1/(Cs*Rc) - 1/(Cs*Ru)];
+B = [   2*Re*Current/Cc;
+        0];
+C = zeros(1,2);
+D = 0;
+
+sys_c = ss(A,B,C,D);
+sys_d = c2d(sys_c, dt);
+
+A_aug = [sys_d.A, zeros(2); eye(2), eye(2)];
+B_aug = [sys_d.B; zeros(2,1)];
+C_aug = [1 0 1 0];
+D_aug = 0;
+end
