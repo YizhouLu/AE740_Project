4a6bc704478989cc471dcdc69a50698fa40488e1
Yizhoulu <luyizhou@umich.edu>
2019-12-12 22:36:35 -0500

    fused MPC into the bus map

---
+++ MPC.m
@@ -0,0 +1,71 @@
+function [X_next, y, ocv, u] = MPC(X0, model, coe)
+%% MPC parameters
+N = struct();
+N.prediction = 10;
+N.control = 1;
+
+penalty = struct();
+penalty.Q = diag([0, 100, 0, 0, 0]); % output = [Vt, e, i, z, Tc]
+penalty.R = 1e-4;                     % input = di
+
+limit = struct();
+% Vt_max = GetOCV(0.8, Temp, model);
+Vt_max = 4.2; % GetOCV(0.8, Temp, model);
+Vt_min = 3.0; % GetOCV(0.2, Temp, model);
+i_min = -getParamESC('QParam',25,model);
+limit.y.max = [Vt_max;  100;     0; 0.9; 50];
+limit.y.min = [Vt_min; -100; i_min;   0;  0];
+limit.du.max =  100;
+limit.du.min = -100;
+
+SOC_setpoint = 0.9;
+X0(7) = X0(3) - SOC_setpoint;
+
+t  = 0;         % initial time
+dt = 1;         % sampling time
+
+
+[A_elec, B_elec, C_elec, D_elec] = generateElecModel(X0(10), X0(3), model, dt, coe);
+[A_thml, B_thml, C_thml, D_thml] = generateThmlModel(X0(10), X0(6), model, dt);
+
+A_aug = blkdiag(A_elec, A_thml);
+B_aug = [B_elec;B_thml];
+C_aug = blkdiag(C_elec, C_thml);
+D_aug = [D_elec;D_thml];
+sys_d = ss(A_aug, B_aug, C_aug, D_aug);
+
+[H, L, G, W, T, ~, ~] = formQPMatrices(sys_d, N, penalty, limit);
+Wtilde = W + T*X0;
+q = L*X0;
+
+options = optimset('Display','off');
+dU = quadprog(H, q, G, Wtilde, [], [], [], [], [], options);
+
+du = dU(1);     % delta u
+u = du + X0(6); % actual control actuation
+
+
+x_ode45_curr = [X0(3); X0(4); X0(10); X0(11)]; % z, Vc, Tc, Ts
+[t_ode45, x_ode45] = ode45(@(t,x) Dynamics(t, x, u, model), t + dt/10 : dt/10 : t + dt, x_ode45_curr);
+x_ode45_next = x_ode45(end,:)';
+t = t_ode45(end);
+
+
+[y,ocv] = Output(X0, u, model, coe);% [Vt, e, i, z, Tc]
+
+X_next = zeros(size(X0));
+X_next(1) = x_ode45_next(1) - X0(3);    % dz_k+1 = z_k+1 - z_k
+X_next(2) = x_ode45_next(2) - X0(4);    % dVc_k+1 = Vc_k+1 - Vc_k
+X_next(3) = x_ode45_next(1);            % z_k+1
+X_next(4) = x_ode45_next(2);            % Vc_k+1
+X_next(5) = 1;                          % 1
+X_next(6) = u;                          % i_k
+X_next(7) = x_ode45_next(1) - SOC_setpoint; % z_k+1 - 0.8
+X_next(8) = x_ode45_next(3) - X0(10);   % dTc_k+1 = Tc_k+1 - Tc_k
+X_next(9) = x_ode45_next(4) - X0(11);   % dTs_k+1 = Ts_k+1 - Ts_k
+X_next(10) = x_ode45_next(3);           % Tc_k+1
+X_next(11) = x_ode45_next(4);           % Ts_k+1
+
+
+
+end
--- Main.m
+++ Main.m
@@ -1,53 +1,59 @@
 close all
-load('MAP.mat')
+load('MAP.mat');
+load('SpeedProfile.mat');
+load('SAMmodel.mat');
+load('SOC_OCV_coe.mat');
 
+%% Simulation parameters
+Nsim = 3000;
+
+%% Figure parameters
+figure
+subplot(1,2,1)
 plot(PATH(1,:),PATH(2,:),'k','linewidth',2);axis equal;grid on;hold on
 
-BusStop_idx = round(linspace(1,1229,20));
+subplot(1,2,2)
+xlabel('Time, s'); title('Bus1 State of Charge'); hold on; grid on; 
+xlim([0 Nsim]); ylim([0 1])
+
+%% Bus stop location
+BusStop_idx = [1, 243246];
 for j = 1:length(BusStop_idx)
+    subplot(1,2,1)
     scatter(PATH(1,BusStop_idx(j)),PATH(2,BusStop_idx(j)),'b','filled')
 end
 
-%% 
+%% Simulation initialization
+Init.Distance     = 0;
 Init.Position_idx = 1;
-Init.SOC = 100;
-Init.State = 0;
-
+Init.isRunning    = 1;
+Init.dz     = 0; 
+Init.dVc    = 0;
+Init.SOC    = 1;
+Init.Vc     = 0;
+Init.i_last = 0;
+Init.e      = 0;
+Init.dTc    = 0;
+Init.dTs    = 0;
+Init.Tc     = 25;
+Init.Ts     = 25;
 Bus1 = Bus(Init);
-Bus2 = Bus(Init);
-Bus3 = Bus(Init);
-tic;
-t_last = 0;
-while true
-    if toc > 0
-        Bus1.updatePosition(PATH);
-        Bus1.updateSOC();
-        Bus1.updateState(BusStop_idx);      
+
+k = 1;
+
+for i = 1:Nsim
+    Bus1.updatePosition(PATH, VELOCITY, i);
+    Bus1.updateSOC();
+    Bus1.updateState(BusStop_idx);
+    
+    if Bus1.isRunning == 0
+        [Bus1.X, y, ocv, u] = MPC(Bus1.X, model, coe);
+        Y(:,k) = y;
+        k = k + 1;
     end
     
-    if toc > 15 
-        Bus2.updatePosition(PATH);
-        Bus2.updateSOC();
-        Bus2.updateState(BusStop_idx);
-    end
-    
-    if toc > 30 
-        Bus3.updatePosition(PATH);
-        Bus3.updateSOC();
-        Bus3.updateState(BusStop_idx);
-    end
-    
-    Bus1.graph();
-    Bus2.graph();
-    Bus3.graph();
-    
-    while toc - t_last < 0.1
-        drawnow;
-    end
-    t_last = toc;
-    
+    Bus1.graph(i, 1, 2);
+    drawnow;
     delete(Bus1.graph_handle);
-    delete(Bus2.graph_handle);
-    delete(Bus3.graph_handle);
 end
 
